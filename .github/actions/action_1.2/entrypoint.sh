0b87d42f21a4d0cad961e0a5d751a7290d2353d8
Dmitrii Overchenko <49155524+n1654@users.noreply.github.com>
2020-09-25 19:05:39 +0300

    Update entrypoint.sh

--- .github/actions/action_1.2/entrypoint.sh
+++ .github/actions/action_1.2/entrypoint.sh
@@ -3,6 +3,11 @@
 echo "EXECUTING PYTHON SCRIPT..."
 python3 /main.py
 ls -lah
+echo "GIT CHECKOUT:"
+git checkout
+echo "GIT ADD log.md:"
 git add log.md
+echo "GIT COMMIT -m "timestamp upadated:"
 git commit -m "timestamp upadated"
+echo "GIT PUSH ORIGIN MASTER:"
 git push origin master
