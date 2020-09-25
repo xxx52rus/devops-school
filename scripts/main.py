c067cf14d077ae0a8a87d019af03809f18977e3c
Dmitrii Overchenko <49155524+n1654@users.noreply.github.com>
2020-09-25 19:15:17 +0300

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,7 @@ print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-with open('log.md', 'w') as f:
+with open('/tmp/log.md', 'w') as f:
     f.write(f'# {timestamp}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
